fc000dc1f6db923f7370f632254409b63ae6a027
Harshrajpal9 <75620896+Harshrajpal9@users.noreply.github.com>
2026-05-30 11:01:20 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
-// const connectDB = require("./config/db");
-
-// const app = express();
-
-// connectDB();
-
-// app.use(express.json());
-// app.use(cookieParser());
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
-
-// app.use("/api/auth", require("./routes/authRoute"));
-// app.use("/api/admin", require("./routes/adminRoute"));
-// app.use("/api/certificates", require("./routes/certificateRoute"));
-
-// app.listen(5000, () => console.log("Server running on 5000"));
-
 
 require("dotenv").config();
 
